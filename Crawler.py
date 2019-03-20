d3ef9bc05fcb5be58fb154b417b77a785e1f3c39
lunarknight00 <zhuolibo@hotmail.com>
2019-03-20 14:44:29 +1100

    Update Crawler.py

--- Crawler.py
+++ Crawler.py
@@ -3,16 +3,33 @@
 # written by Henry Libo Zhuo
 # 2019-03-13
 
-import time
-import pandas as pd
-from selenium import webdriver
 from bs4 import BeautifulSoup
+from selenium import webdriver
+import requests
+import pandas as pd
+import re,time,csv
 
 class WebCMS:
-    def __init__(self,username,password,path):
+    base = "https://webcms3.cse.unsw.edu.au"
+    def __init__(self,username,password,path,tag = "forum",courseCode,semster,year):
         self.username = username
         self.password = password
         self.driver = webdriver.Chrome(path)
+        self.tag = self.genTags()
+        self.year = year
+        self.semster = semster
+        self.courseCode = courseCode
+        self.vips = self.extractVIP()
+        self.tags = self.
+        self.questions, self.replies = self.crawl()
+        self.Index = self.Index()
+        self.SecondIndex = self.SecondIndex()
+
+    def courseURL(self):
+        return "https://webcms3.cse.unsw.edu.au/" + self.courseCode + '/' + self.year + 's'+ self.year +"/"
+
+    def genTags(self):
+        return len(self.questions)
 
     def login(self):
         driver = self.driver
@@ -25,18 +42,103 @@ class WebCMS:
             time.sleep(0.2)
         time.sleep(10)
         driver.find_element_by_xpath("//*[@id='webcms3top']/div/div[1]/div/div[2]/div/div/form/button").click()
-        # TO DO brower the forums website 
-        
-        # helper function to fetch a table in the website
-        table = self.getTable(driver)
+        return self.crawl()
+    
+    def Index(self):
+        driver = self.driver
+        driver.get(self.courseURL(self) +"forums/")
+        page = driver.page_source
+        soup = BeautifulSoup(page,"html.parser")
+        table = soup.find_all("tr")
+        forum = []
+        for row in table:
+            cols = row.find_all("td")
+            for col in cols:
+                try:
+                    nb = int(col.get_text())
+                    if nb > 0:
+                        tmp = row.find("a").get("href")
+                        if  re.findall("forums",tmp) and len(tmp) > 22:
+                            forum.append(base + tmp)
+                except:
+                    continue
+        return forum
 
-        
-        # TO DO scrapy data from different forums, bfs
-        # TO DO data clean
-        # TO DO out put data with csv
-        return table
+    def SecondIndex(self):
+        driver = self.driver
+        visited = set()
+        forum1 = []
+        for url in self.Index:
+            driver.get(url)
+            page = driver.page_source
+            soup = BeautifulSoup(page,"html.parser")
+            links = soup.find_all("a")
+            for link in links:
+                tmp = link.get("href")
+                if tmp in visited:
+                    continue
+                visited.add(tmp)
+                if  re.findall("forums",tmp) and len(tmp) > 22:
+                    forum1.append(base + tmp)
+        return forum1
 
-    def getTable(self,driver):
+    def crawl(self.):
+        driver = self.driver
+        questions,replies,visited = [],[],set()
+        for url in forum1:
+            if url in visited:
+                continue
+            visited.add(url)
+            driver.get(url)
+            time.sleep(5)
+            page = driver.page_source
+            soup = BeautifulSoup(page,"html.parser")
+            reply = str()
+            question = str()
+            aset = soup.find_all("div",{"class":"comment-content"})
+            reply =str()
+            for tmp in aset:
+                person = tmp.find("header").find("a").get_text()
+                text = tmp.get_text()
+                lines = [line.strip() for line in text.splitlines()]
+                comment = [line for line in lines[1:len(lines)-1] if len(line)>0]
+                try:
+                    img = tmp.find("img").get("src")
+                    comment.append(base + img)
+                except:
+                    pass
+                if person in vips:
+                    reply += " " + " ".join(comment)
+                else:
+                    question += " " + " ".join(comment)
+            if len(reply) > 0:
+                if question not in set(questions) and reply not in set(replies):
+                    questions.append(question)
+                    replies.append(reply)
+        return questions,replies
+
+    def output(self,filename):
+        self.tags = [self.tag for _ in range(len(questions))]
+        with open(filename, 'w', newline='',encoding='utf-8') as csvfile:
+            fieldnames = ["Source", "Question", "reply"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for s, q, r in zip(self.tags, self.questions, self.replies):
+                writer.writerow([s, q, r])
+
+    def extractVIP(self):
+        driver = self.driver
+        html = driver.page_source
+        soup = BeautifulSoup(html,"lxml")
+        vips = []
+        for p in soup.find_all("li",{"class":"list-group-item"}):
+            if p.find("span",{"class":"label label-primary"}):
+                vip = p.find("a").get_text()
+                vips.append(vip)
+        return vips
+
+    def getTable(self):
+        driver = self.driver
         html = driver.page_source
         soup = BeautifulSoup(html,"lxml")
         table = soup.find_all("table")
