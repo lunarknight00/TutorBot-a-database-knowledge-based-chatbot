71b2342d19a42f66cd35729a734b95dbf98fc7ab
YimingDong <30427155+YimingDong@users.noreply.github.com>
2019-03-27 14:57:36 +1100

    Add files via upload

--- test1.py
+++ test1.py
@@ -1,6 +1,7 @@
 import dialogflow
 import apiai
 import json
+import speech_recognition as sr 
 #import request
 
 import dialogflow_v2
@@ -19,13 +20,43 @@ def reply(msg_content, user_id):
 	s = json.loads(response.read().decode('utf-8'))
 	
 	if s['result']['action'] == 'input.unknown':
-		raise Exception('api.ai cannot reply this message')
+		print("Sorry could't understand your question.")
 	if s['status']['code'] == 200:
 		print("use APIAI reply")
 		print(str(s['result']['fulfillment']['speech']))
+		#raise Exception('api.ai cannot reply this message')
                 #return s['result']['fulfillment']['speech'] 
+
+def voice_iden():
+	r = sr.Recognizer()               
+	with sr.Microphone() as source: 
+		print("Speak Anything :")
+		audio = r.listen(source)    
+		try:
+			text = r.recognize_google(audio)    
+			print("You said : {}".format(text))
+		except:
+			print("Sorry could not recognize your voice")
+	return text
+	'''
+	while(text != 'quiet'):
+		reply(text, 1)
+		r = sr.Recognizer()                 
+		with sr.Microphone() as source:     
+			print("Speak Anything :")
+			audio = r.listen(source)        
+			try:
+				text = r.recognize_google(audio)    
+				print("You said : {}".format(text))
+			except:
+				print("Sorry could not recognize your voice") 
+		#reply(text, 1).
+	print("See you")
+	'''
 
 question = input("Enter your question: ")
 while(question != 'quit'):
+	if question == 'voice' :
+		question = voice_iden()
 	reply(question, 1)
-	question = input("Enter your question: ")
+	question = input("Enter your question ")
