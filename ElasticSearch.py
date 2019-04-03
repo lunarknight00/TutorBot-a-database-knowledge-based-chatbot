15427d31a53dc2cd8d6b9b6930dfb6f597cf186c
Roy <358963981@qq.com>
2019-04-04 10:51:09 +1100

    Update ElasticSearch.py

--- ElasticSearch.py
+++ ElasticSearch.py
@@ -2,12 +2,12 @@ import json
 import requests
 
 
-def match_search(keyword, operator="or",msm="50%",top_n=3,link="http://35.231.149.211:9200/qaset/COMP9311/_search"): #msm:minimum_should_match
+def match_search(keyword, operator="or",fuzziness="AUTO",msm="50%",top_n=3,link="http://35.231.149.211:9200/qaset/COMP9311/_search"): #msm:minimum_should_match
     """
     settings about msm:
     https://www.elastic.co/guide/en/elasticsearch/reference/5.6/query-dsl-minimum-should-match.html#query-dsl-minimum-should-match
     """
-    data_dic = {"query":{"match":{"question":{"query":keyword,"operator":operator,"minimum_should_match":msm}}}}
+    data_dic = {"query":{"match":{"question":{"query":keyword,"operator":operator,"minimum_should_match":msm,"fuzziness":fuzziness}}}}
     r = requests.get(link, data=json.dumps(data_dic),headers={'Content-Type':'application/json'})
 
     response = json.loads(r.text)
