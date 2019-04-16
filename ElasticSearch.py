92b10ff1ba0229609c212f670d9ad1a5444854d4
Roy <358963981@qq.com>
2019-04-16 13:27:23 +1000

    Update ElasticSearch.py

--- ElasticSearch.py
+++ ElasticSearch.py
@@ -1,13 +1,20 @@
 import json
 import requests
+from nltk.corpus import stopwords
 
+keyword_filter = lambda x:' '.join([word for word in x.split() if word not in (stopwords.words('english'))])
 
-def match_search(keyword, operator="or",fuzziness="AUTO",msm="50%",top_n=3,link="http://35.231.149.211:9200/qaset/COMP9311/_search"): #msm:minimum_should_match
+def match_search(keyword, operator="or",msm="50%",top_n=3,
+                 link="http://35.231.149.211:9200/qaset/COMP9311/_search",
+                keyword_filter=None): #msm:minimum_should_match
     """
     settings about msm:
     https://www.elastic.co/guide/en/elasticsearch/reference/5.6/query-dsl-minimum-should-match.html#query-dsl-minimum-should-match
     """
-    data_dic = {"query":{"match":{"question":{"query":keyword,"operator":operator,"minimum_should_match":msm,"fuzziness":fuzziness}}}}
+    if keyword_filter:
+        keyword = keyword_filter(keyword)
+    
+    data_dic = {"query":{"match":{"question":{"query":keyword,"operator":operator,"minimum_should_match":msm}}}}
     r = requests.get(link, data=json.dumps(data_dic),headers={'Content-Type':'application/json'})
 
     response = json.loads(r.text)
@@ -15,4 +22,6 @@ def match_search(keyword, operator="or",fuzziness="AUTO",msm="50%",top_n=3,link=
     max_score = response["hits"]["max_score"]
     top_doc = response["hits"]["hits"][:top_n]
     return total_find,max_score,top_doc
-print(match_search("following"))
+
+
+print(match_search("what the fuck",msm="50%"))
