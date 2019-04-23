55d2171b2a3d00e2e718118925db0bba4eef6f37
Roy <358963981@qq.com>
2019-04-23 12:39:36 +1000

    Set public access

--- final-version/comp9900_run.py
+++ final-version/comp9900_run.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 def home():
 	return render_template('index.html')
 
-app.run(port=5001,debug=True)
+app.run(host='0.0.0.0',port=5001,debug=True)
--- final-version/js/botapi.py
+++ final-version/js/botapi.py
@@ -96,4 +96,4 @@ class es(Resource):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=5000,debug=True)
